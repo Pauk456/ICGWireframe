965a7c58e1a2a1c93a00537584c41ff18bdc1ca4
Vova <456pauk@mail.ru>
2025-04-29 03:03:34 +0700

    d

---
+++ Controller/BSpline.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+public class BSpline
+{
+    public List<Point> points;
+    private static readonly double[,] Ms = new double[4, 4]
+    {
+        { -1.0/6,  3.0/6, -3.0/6, 1.0/6 },
+        {  3.0/6, -6.0/6,  3.0/6, 0.0/6 },
+        { -3.0/6,  0.0/6,  3.0/6, 0.0/6 },
+        {  1.0/6,  4.0/6,  1.0/6, 0.0/6 }
+    };
+
+    public int CountApproachingLines { get; set; } = 20;
+
+    public BSpline()
+    {
+        points = new List<Point>();
+    }
+
+    public void AddPoint(double x, double y)
+    {
+        points.Add(new Point(x, y));
+    }
+
+    public List<Point> GetBSpline()
+    {
+        if (points.Count < 4)
+            return new List<Point>(points);
+
+        var result = new List<Point>();
+
+        for (int i = 1; i < points.Count - 2; i++)
+        {
+            Point p0 = points[i - 1];
+            Point p1 = points[i];
+            Point p2 = points[i + 1];
+            Point p3 = points[i + 2];
+
+            for (int j = 0; j <= CountApproachingLines; j++)
+            {
+                double t = (double)j / CountApproachingLines;
+                double t2 = t * t;
+                double t3 = t2 * t;
+
+                double b0 = (-t3 + 3 * t2 - 3 * t + 1) / 6;
+                double b1 = (3 * t3 - 6 * t2 + 4) / 6;
+                double b2 = (-3 * t3 + 3 * t2 + 3 * t + 1) / 6;
+                double b3 = t3 / 6;
+
+                double x = b0 * p0.X + b1 * p1.X + b2 * p2.X + b3 * p3.X;
+                double y = b0 * p0.Y + b1 * p1.Y + b2 * p2.Y + b3 * p3.Y;
+
+                result.Add(new Point(x, y));
+            }
+        }
+
+        return result;
+    }
+
+    public List<Point> Get3DFigure()
+    {
+       
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,127 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ICGWireframe
+
+namespace ICGWireframe;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private BSpline bSpline = new BSpline();
+    private Polyline splineLine = new Polyline()
     {
-        public MainWindow()
+        Stroke = Brushes.Red,
+        StrokeThickness = 2
+    };
+
+    //private Polyline ConnectingLine = new Polyline() 
+    //{
+    //    Stroke = Brushes.Gray,
+    //    StrokeThickness = 2
+    //};
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        MainCanvas.Children.Add(splineLine);
+        bSpline.CountApproachingLines = int.Parse(SegmentsTextBox.Text);
+    }
+
+    private void ApplyButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (int.TryParse(SegmentsTextBox.Text, out int segments) && segments > 0)
         {
-            InitializeComponent();
+            bSpline.CountApproachingLines = segments;
+            UpdateBSpline();
         }
+        else
+        {
+            MessageBox.Show("Please enter a positive integer value", "Invalid input",
+                          MessageBoxButton.OK, MessageBoxImage.Warning);
+            SegmentsTextBox.Text = bSpline.CountApproachingLines.ToString();
+        }
+    }
+
+    private void UpdateBSpline()
+    {
+        bSpline.points.Clear();
+
+        foreach (var child in MainCanvas.Children)
+        {
+            if (child is Ellipse ellipse)
+            {
+                double x = Canvas.GetLeft(ellipse) + ellipse.Width / 2;
+                double y = Canvas.GetTop(ellipse) + ellipse.Height / 2;
+                bSpline.AddPoint(x, y);
+            }
+        }
+
+        splineLine.Points.Clear();
+        foreach (var point in bSpline.GetBSpline())
+        {
+            splineLine.Points.Add(point);
+        }
+    }
+
+    private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.Source is Ellipse ellipse)
+        {
+            ellipse.CaptureMouse();
+            ellipse.Fill = Brushes.Red;
+        }
+    }
+
+    private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (e.Source is Ellipse ellipse && ellipse.IsMouseCaptured)
+        {
+            ellipse.ReleaseMouseCapture();
+            ellipse.Fill = Brushes.Blue;
+            UpdateBSpline();
+        }
+    }
+
+    private void Canvas_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (e.Source is Ellipse ellipse && ellipse.IsMouseCaptured && e.LeftButton == MouseButtonState.Pressed)
+        {
+            var canvas = sender as Canvas;
+            var position = e.GetPosition(canvas);
+            Canvas.SetLeft(ellipse, position.X - ellipse.Width / 2);
+            Canvas.SetTop(ellipse, position.Y - ellipse.Height / 2);
+            UpdateBSpline();
+        }
+    }
+
+    private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var canvas = sender as Canvas;
+        var position = e.GetPosition(canvas);
+
+        if (e.Source is Ellipse ellipse)
+        {
+            canvas.Children.Remove(ellipse);
+        }
+        else
+        {
+            var dot = new Ellipse
+            {
+                Width = 20,
+                Height = 20,
+                Fill = Brushes.Blue,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1
+            };
+
+            Canvas.SetLeft(dot, position.X - dot.Width / 2);
+            Canvas.SetTop(dot, position.Y - dot.Height / 2);
+            canvas.Children.Add(dot);
+        }
+
+        UpdateBSpline();
     }
 }
