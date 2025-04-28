93c75b6d7f31246142a11d21e9f8c97bd1a93324
Vova <456pauk@mail.ru>
2025-04-28 19:48:44 +0700

    First commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ICGWireframe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
